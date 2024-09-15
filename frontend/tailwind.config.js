c397668142e01e09f4cfd9d60f4b79761e191cfd
praneeth-pixzarloop <pereradilshan594@gmail.com>
2024-09-15 12:57:12 +0530

    Duplicate Object Property fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,19 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-
-  theme: {
     extend: {
       colors: {
         themeBlue: '#27005D',
         themePurple: '#9400FF',
         themeLightBlue: '#AED2FF',
         themeLightGray: '#E4F1FF',
-      },
-    },
-  },
+      },
+    },
+  },
+  plugins: [],
 }
-
